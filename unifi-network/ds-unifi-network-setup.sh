e1fd5701c51976ae10a36d0c67eef8b623cd4c67
Christopher Dobler <chrisdobler@gmail.com>
2019-04-18 00:25:11 -0700

    unifi network boiler

---
+++ unifi-network/ds-unifi-network-setup.sh
@@ -0,0 +1,4 @@
+# sudo apt-get install nagios-nrpe-server nagios-plugins openssl nagios-nrpe-plugin nagios-plugins-extra nagios-plugins-basic
+# sudo vim /etc/nagios/nrpe.cfg
+
+sudo apt-get install -y unifi
