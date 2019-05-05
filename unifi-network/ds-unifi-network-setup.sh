c6831689194863d11c90f818c88950dbbbb3c780
Christopher Dobler <chrisdobler@gmail.com>
2019-05-05 09:40:32 -0700

    adds pi-hole configuration

--- unifi-network/ds-unifi-network-setup.sh
+++
@@ -1,4 +0,0 @@
-# sudo apt-get install nagios-nrpe-server nagios-plugins openssl nagios-nrpe-plugin nagios-plugins-extra nagios-plugins-basic
-# sudo vim /etc/nagios/nrpe.cfg
-
-sudo apt-get install -y unifi
