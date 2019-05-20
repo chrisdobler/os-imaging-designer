c1e516c4278611d9f287086f9da6aa3a77d27279
Christopher Dobler <chrisdobler@gmail.com>
2019-05-20 00:10:30 -0700

    updates pihole network

--- pi-hole/pi-hole-setup.sh
+++ pi-hole/pi-hole-setup.sh
@@ -1,1 +1,9 @@
-curl -sSL https://install.pi-hole.net | bash
+sudo apt-get install -y curl git
+
+# normal install
+sudo curl -sSL https://install.pi-hole.net | bash
+
+#auto install
+# git clone https://github.com/pi-hole/pi-hole.git
+
+# sudo pi-hole/automated\ install/basic-install.sh
