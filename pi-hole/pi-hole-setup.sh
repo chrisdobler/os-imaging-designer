071e940b7c1514997418f412cca5170505a968f2
Christopher Dobler <chrisdobler@gmail.com>
2019-05-20 00:35:51 -0700

    adds unattended pihole

--- pi-hole/pi-hole-setup.sh
+++ pi-hole/pi-hole-setup.sh
@@ -1,9 +1,6 @@
 sudo apt-get install -y curl git
 
-# normal install
+sudo mkdir /etc/pihole
+sudo mv -v /home/user/setupVars.conf /etc/pihole/
+
 sudo curl -sSL https://install.pi-hole.net | bash
-
-#auto install
-# git clone https://github.com/pi-hole/pi-hole.git
-
-# sudo pi-hole/automated\ install/basic-install.sh
