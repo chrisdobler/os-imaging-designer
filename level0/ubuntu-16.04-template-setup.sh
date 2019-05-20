a56988de69f67d730e9623ee4de25787e4d95093
Christopher Dobler <chrisdobler@gmail.com>
2019-05-19 23:28:20 -0700

    adds curl,git

--- level0/ubuntu-16.04-template-setup.sh
+++ level0/ubuntu-16.04-template-setup.sh
@@ -2,7 +2,7 @@ sudo rm /var/lib/dpkg/lock
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y vim htop
+sudo apt-get install -y vim htop curl git
 
 sudo mkdir .ssh
 sudo mv id_rsa.pub .ssh/authorized_keys
