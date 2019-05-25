74979081d18d63d63edb8801633a2fb2ef8912df
Christopher Dobler <chrisdobler@gmail.com>
2019-05-25 13:24:23 -0700

    updates pihole setup script

--- pi-hole/pi-hole-setup.sh
+++ pi-hole/pi-hole-setup.sh
@@ -1,7 +1,13 @@
+sudo apt-get update
 sudo apt-get install -y curl git
 
-sudo mv -v /home/user/pihole/ /etc/
+sudo mkdir /etc/pihole
+sudo mv -v /home/user/pihole/setupVars.conf /etc/pihole/
 
-git clone https://github.com/pi-hole/pi-hole.git
+git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
 
-sudo pi-hole/automated\ install/basic-install.sh --unattended
+sudo /etc/.pihole/automated\ install/basic-install.sh --unattended
+
+# sudo mv -v /home/user/pihole/ /etc/
+rsync -av /home/user/pihole/ /etc/pihole/
+rm -rf /home/user/pihole/
