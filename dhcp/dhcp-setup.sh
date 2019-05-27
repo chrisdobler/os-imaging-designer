18b889735c50440de814fd57469e2a65c8852107
Christopher Dobler <chrisdobler@gmail.com>
2019-05-26 18:13:59 -0700

    updates dhcp setup script

--- dhcp/dhcp-setup.sh
+++ dhcp/dhcp-setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get remove dhcp-client
-# sudo apt-get install -y isc-dhcp-server
+sudo apt-get install -y isc-dhcp-server
 
-# sudo rsync -av /home/user/dhcp/ /etc/dhcp/
-# rm -rf /home/user/dhcp/j
+sudo rsync -av /home/user/dhcp/ /etc/dhcp/
+rm -rf /home/user/dhcp/
