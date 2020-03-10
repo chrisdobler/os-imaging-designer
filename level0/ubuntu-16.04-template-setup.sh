bb1ea38da442ea53459609655d07216572ca768b
Christopher Dobler <chrisdobler@gmail.com>
2020-03-10 02:33:40 -0700

    template updates

--- level0/ubuntu-16.04-template-setup.sh
+++ level0/ubuntu-16.04-template-setup.sh
@@ -10,3 +10,9 @@ sudo mv id_rsa.pub .ssh/authorized_keys
 # todo: remove splash screen
 # sudo sed line10 GRUB_CMDLINE_LINUX_DEFAULT=""
 # sudo update-grub
+
+# force fsck on every boot
+# ubuntu will lock the boot partition if any errors are detected. this saves you.
+# /etc/fstab: static file system information.
+# sudo fdisk -l
+sudo tune2fs -c 1 /dev/sdXY
