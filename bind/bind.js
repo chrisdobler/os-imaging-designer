a56eb90c6f29f1c1aab15f2c09f86c900951bc9e
Christopher Dobler <chrisdobler@gmail.com>
2020-05-15 18:11:42 -0700

    adds bind server

---
+++ bind/bind.js
@@ -0,0 +1,10 @@
+export default {
+  mode: 'level2',
+  machineType: 'ubuntu-16.04',
+  supportsInitialBuild: false,
+  overrides: {
+    ram: 1024,
+  },
+  buildType: 'packer',
+  supportFiles: false,
+};
