c473c579558dbcd21006ecd5f61e0fa5247b6cd3
Christopher Dobler <chrisdobler@gmail.com>
2020-05-30 19:22:16 -0700

    updates minecraft

--- minecraft/minecraft.js
+++ minecraft/minecraft.js
@@ -1,24 +1,19 @@
 export default {
   mode: 'level2',
   machineType: 'ubuntu-16.04',
-  builder: ({ targetPlatform, machineTypeSpecific, options = {} }) => {
-    const base = {
-      builders: [
-        {
-          ...(() => (targetPlatform === 'vmware-workstation' ? {} : {}))(),
-        },
-      ],
-      provisioners: [
-        ...machineTypeSpecific,
-        {
-          type: 'shell',
-          script: `${process.cwd()}/minecraft/minecraft-setup.sh`,
-          execute_command:
-            "echo '{{user `ubuntu_template_password`}}' | sudo -S sh -c '{{ .Vars }} {{ .Path }}'",
-        },
-      ],
-    };
-
-    return base;
-  },
+  builder: ({ targetPlatform }) => ({
+    builders: [
+      {
+        ...(() => (targetPlatform === 'vmware-workstation' ? {} : {}))(),
+      },
+    ],
+    provisioners: [
+      {
+        type: 'shell',
+        script: `${process.cwd()}/minecraft/minecraft-setup.sh`,
+        execute_command:
+          "echo '{{user `ubuntu_template_password`}}' | sudo -S sh -c '{{ .Vars }} {{ .Path }}'",
+      },
+    ],
+  }),
 };
